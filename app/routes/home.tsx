497fe35df5c8abaf3ee8c23d2fe65c4fab18436e
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 <sabaudianin@gmail.com>
2025-02-04 20:15:41 +0100

    Add dev dependencies

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Hero } from "~/Hero/Hero";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "React Router App" },
+    { name: "description", content: "Welcome t0000000o React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <Hero />
+      <Welcome />
+    </>
+  );
 }
